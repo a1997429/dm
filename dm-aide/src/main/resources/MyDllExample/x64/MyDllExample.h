bbf6bff138ef1fa3916adfd7d368877102ecb856
Musk <68292463+imusk@users.noreply.github.com>
2021-06-03 01:59:44 +0800

    add: MyDllExample示例

---
+++ dm-aide/src/main/resources/MyDllExample/x64/MyDllExample.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// https://blog.csdn.net/Bryan_QAQ/article/details/92798098
+
+_declspec(dllexport) int add(int a, int b);
+
+_declspec(dllexport) int max(int a, int b);
+
---
+++ dm-aide/src/main/resources/MyDllExample/x86/MyDllExample.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// ²Î¿¼: https://blog.csdn.net/Bryan_QAQ/article/details/92798098
+
+_declspec(dllexport) int add(int a, int b);
+
+_declspec(dllexport) int max(int a, int b);
+
